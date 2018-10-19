05f0c40f207c8e1f00a10c9513a1859da73e3424
Ahmed Amine CHAARI <aachaari@expertime.com>
2018-10-19 19:29:41 +0200

    sp

--- ApiFlask/settings.py
+++ ApiFlask/settings.py
@@ -1,6 +1,8 @@
 from flask import Flask
+from flask_cors import CORS
 
 app = Flask(__name__)
+cors = CORS(app)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///books.sqlite3"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
